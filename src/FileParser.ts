30c27c874ba50310aeb277527157131fda1fe586
Mathias Lorenzen <mathias.lorenzen@live.com>
2017-05-14 13:35:41 +0200

    outer scope.

--- src/FileParser.ts
+++ src/FileParser.ts
@@ -1,5 +1,24 @@
 ﻿export class FileParser {
 
+    constructor(private contents: string) {
 
+    }
+
+    parseFile(): CSharpFile {
+        var file = <CSharpFile>{};
+        this.parseUsings(file);
+
+        return file;
+    }
+
+    private getOuterScopeContents() {
+        var regex = /.*?\{(.*)\}.*?/gm;
+        var match = regex.(this.contents)[0];
+        match.
+    }
+
+    private parseUsings(file: CSharpFile) {
+
+    }
 
 }
