5876307ec5e57b42eeb680312f1dffc248080f66
Mathias Lorenzen <mathias.lorenzen@live.com>
2017-05-18 01:38:54 +0200

    update for csharp parser.

---
+++ src/Index.js
@@ -0,0 +1,18 @@
+"use strict";
+var ClassParser_1 = require("./ClassParser");
+exports.ClassParser = ClassParser_1.ClassParser;
+var EnumParser_1 = require("./EnumParser");
+exports.EnumParser = EnumParser_1.EnumParser;
+var FileParser_1 = require("./FileParser");
+exports.FileParser = FileParser_1.FileParser;
+var MethodParser_1 = require("./MethodParser");
+exports.MethodParser = MethodParser_1.MethodParser;
+var NamespaceParser_1 = require("./NamespaceParser");
+exports.NamespaceParser = NamespaceParser_1.NamespaceParser;
+var RegExHelper_1 = require("./RegExHelper");
+exports.RegExHelper = RegExHelper_1.RegExHelper;
+var ScopeHelper_1 = require("./ScopeHelper");
+exports.ScopeHelper = ScopeHelper_1.ScopeHelper;
+var UsingsParser_1 = require("./UsingsParser");
+exports.UsingsParser = UsingsParser_1.UsingsParser;
+//# sourceMappingURL=Index.js.map
---
+++ src/Index.ts
@@ -0,0 +1,8 @@
+﻿export { ClassParser } from './ClassParser';
+export { EnumParser } from './EnumParser';
+export { FileParser } from './FileParser';
+export { MethodParser } from './MethodParser';
+export { NamespaceParser } from './NamespaceParser';
+export { RegExHelper } from './RegExHelper';
+export { ScopeHelper } from './ScopeHelper';
+export { UsingsParser } from './UsingsParser';
