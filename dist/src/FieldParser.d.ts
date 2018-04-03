3b7bbd53768fc068fedd197c79caaf75971fab3a
Mathias Lorenzen <mll@ipaper.io>
2018-04-03 12:35:08 +0200

    removed dist folder.

--- dist/src/FieldParser.d.ts
+++
@@ -1,9 +0,0 @@
-import { CSharpField } from './Models';
-import { TypeParser } from './TypeParser';
-export declare class FieldParser {
-    private typeParser;
-    private scopeHelper;
-    private regexHelper;
-    constructor(typeParser: TypeParser);
-    parseFields(content: string): CSharpField[];
-}
--- dist/src/UsingsParser.d.ts
+++
@@ -1,7 +0,0 @@
-import { CSharpUsing } from './Models';
-export declare class UsingsParser {
-    private scopeHelper;
-    private regexHelper;
-    constructor();
-    parseUsings(content: string): CSharpUsing[];
-}
