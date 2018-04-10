c64a510efdd9d0741a89f82e429ac59d502dbbd5
Mathias Lorenzen <mll@ipaper.io>
2018-04-10 10:30:14 +0200

    added tests to assert regex safety.

---
+++ spec/RegExHelperSpec.ts
@@ -0,0 +1,43 @@
+import { RegExHelper } from '../src/RegExHelper';
+import isSafeRegEx = require('safe-regex');
+
+describe("RegExHelper", function () {
+
+    var regexHelper = new RegExHelper();
+
+    var assertIsSafe = (name: keyof RegExHelper) => {
+        it("should never time out for the regular expression " + name, () => {
+            expect(isSafeRegEx(regexHelper[name].call(regexHelper))).toBe(true);
+        });
+    };
+
+    assertIsSafe('getAttributeParameterRegex');
+    assertIsSafe('getAttributeParametersRegex');
+    assertIsSafe('getAttributeRegex');
+    assertIsSafe('getAttributesRegex');
+    assertIsSafe('getClassRegex');
+    assertIsSafe('getEnumOptionRegex');
+    assertIsSafe('getEnumRegex');
+    assertIsSafe('getGenericNameRegex');
+    assertIsSafe('getGenericTypeNameRegex');
+    assertIsSafe('getGenericTypeWrapperRegex');
+    assertIsSafe('getInterfaceRegex');
+    assertIsSafe('getKeywordRegex');
+    assertIsSafe('getLiteralCharacterRegex');
+    assertIsSafe('getLiteralNumberDecimalRegex');
+    assertIsSafe('getLiteralNumberHexadecimalRegex');
+    assertIsSafe('getLiteralNumberIntegerRegex');
+    assertIsSafe('getLiteralNumberRegex');
+    assertIsSafe('getLiteralRegex');
+    assertIsSafe('getLiteralStringRegex');
+    assertIsSafe('getMethodParameterRegex');
+    assertIsSafe('getMethodParametersWrapperRegex');
+    assertIsSafe('getMethodRegex');
+    assertIsSafe('getModifierRegex');
+    assertIsSafe('getModifiersRegex');
+    assertIsSafe('getNameRegex');
+    assertIsSafe('getPropertyRegex');
+    assertIsSafe('getSignRegex');
+    assertIsSafe('getTypeConstraintRegex');
+
+});
